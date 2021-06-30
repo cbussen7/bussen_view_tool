9ae328d7df7bfe3b250c3f2fe27484ab44faeb77
Christopher Bussen <cbussen7@gmail.com>
2021-06-30 11:35:47 -0500

    View tool initial commit

---
+++ lib/bussen_view_tool.rb
@@ -0,0 +1,8 @@
+require "bussen_view_tool/version"
+require "bussen_view_tool/renderer"
+
+module BussenViewTool
+  class Error < StandardError; end
+
+  # Your code goes here...
+end
---
+++ lib/bussen_view_tool/version.rb
@@ -0,0 +1,3 @@
+module BussenViewTool
+  VERSION = "0.1.0"
+end
